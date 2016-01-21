9886f4d555e68af0e836eaf21449d91a17e82402
Kudrik <kudrik.ru@gmail.com>
2016-01-21 20:27:06 +0700

    Preparing project workplace

--- components/Html/Html.js
+++ components/Html/Html.js
@@ -1,11 +1,8 @@
 /**
- * React Static Boilerplate
- * https://github.com/koistya/react-static-boilerplate
- * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
+ * Test for Scentbird by kudrik.ru@gmail.com
  */
 
 import React, { Component, PropTypes } from 'react';
-import GoogleAnalytics from '../GoogleAnalytics';
 import { title, description } from '../../config';
 
 class Html extends Component {
@@ -14,7 +11,7 @@ class Html extends Component {
     title: PropTypes.string,
     description: PropTypes.string,
     body: PropTypes.string.isRequired,
-    debug: PropTypes.bool.isRequired,
+    debug: PropTypes.bool.isRequired
   };
 
   render() {
@@ -27,12 +24,10 @@ class Html extends Component {
         <meta name="description" content={this.props.description || description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" />
         <script src={'/app.js?' + new Date().getTime()}></script>
       </head>
       <body>
         <div id="app" dangerouslySetInnerHTML={{__html: this.props.body}} />
-        <GoogleAnalytics />
       </body>
       </html>
     );
--- config.js
+++ config.js
@@ -5,8 +5,8 @@
  */
 
 export default {
-  title: 'React Static Boilerplate',
-  description: 'Static website generator for React.js web applications.',
+  title: 'Scentbird: Profile',
+  description: 'Profile page at Scentbird',
   googleAnalyticsId: 'UA-XXXXX-X',
 };
 
